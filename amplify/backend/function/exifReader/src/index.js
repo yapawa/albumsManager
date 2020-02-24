1b760b418eb14dbddd954af56fe6a1b07850ca14
Daniel Muller <github@daniel.ctrlaltdel.ch>
2020-02-24 17:44:03 +0800

    Parse and store exif data

--- amplify/backend/function/exifReader/src/index.js
+++ amplify/backend/function/exifReader/src/index.js
@@ -5,13 +5,40 @@ var region = process.env.REGION
 var storageOriginalsBucketName = process.env.STORAGE_ORIGINALS_BUCKETNAME
 
 Amplify Params - DO NOT EDIT */
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3()
+const ExifReader = require('exifreader')
 
 exports.handler = async (event) => {
   console.log(JSON.stringify(event, null, 2))
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
+
+  if (event.s3 && event.s3.object && event.s3.object.key) {
+    const params = {
+      Key: decodeURIComponent(event.s3.object.key),
+      Bucket: event.s3.bucket.name
+    }
+    return S3.getObject(params).promise()
+      .then(data => {
+        try {
+          return ExifReader.load(data.Body)
+        } catch (error) {
+          console.log('Error: ' + error.message)
+          return null
+        }
+      })
+      .then(exif => {
+        const exifParams = {
+          Key: `${decodeURIComponent(event.s3.object.key)}.exif`,
+          Bucket: event.s3.bucket.name,
+          ContentType: 'application/json',
+          Body: JSON.stringify(exif, null, 2)
+        }
+        return S3.putObject(exifParams).promise()
+      })
+      .catch(err => {
+        console.error(err)
+        return false
+      })
   }
-  return response
+  return true
 }
