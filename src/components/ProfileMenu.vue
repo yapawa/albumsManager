ba772f1fbccd09409c6c53955db6c9f8078631c9
Daniel Muller <github@daniel.ctrlaltdel.ch>
2020-03-05 15:22:45 +0800

    Add "Build all" action

--- src/components/ProfileMenu.vue
+++ src/components/ProfileMenu.vue
@@ -9,6 +9,12 @@
           q-item-section
             q-item-label {{ $t('Profile') }}
             q-item-label(caption) {{ $t('View your profile') }}
+        q-item(clickable v-ripple @click="apiBuildAll")
+          q-item-section(avatar)
+            q-icon(name="publish")
+          q-item-section
+            q-item-label {{ $t('Generate API Data') }}
+            q-item-label(caption) {{ $t('Build all Albums') }}
         q-item(clickable v-ripple @click="signOut")
           q-item-section(avatar)
             q-icon(name="exit_to_app")
@@ -17,8 +23,13 @@
 </template>
 
 <script>
+import BuildApi from 'src/mixins/buildApi'
+
 export default {
   name: 'ProfileMenu',
+  mixins: [
+    BuildApi
+  ],
   methods: {
     signOut: function (event) {
       this.$Amplify.Auth.signOut()
